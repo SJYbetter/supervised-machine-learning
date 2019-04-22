b8ed96ae34c5160af078dfe2fc1d771df31f0f9a
SJYbetter <sjyhyn@gmail.com>
2019-04-22 09:14:19 -0400

    add .

--- TwitterDataAnalysis/textmining/login.py
+++
@@ -1,8 +0,0 @@
-import tweepy
-
-auth = tweepy.OAuthHandler('GNBLYoaGVNSV8biVYSpBkJKOB', 'b4qlV2dnGpXvWSmy6u96MYTqMgvJoxkXgxSgJeom4j9LTZcPmd')
-auth.set_access_token('949819789-1DhjAAlbkFSDZZQqCRrbkb25iAjSDx8ivPa4MjMD',
-                      'GkTPYFmY7J0vrZDSp1LTIK8suCmySXKHOHtj0tTeeyXqj')
-
-# ref: https://developer.twitter.com/en/use-cases/analyze
-api = tweepy.API(auth, proxy='socks5://127.0.0.1:998')
